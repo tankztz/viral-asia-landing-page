8e1f5bc49d7d616f32c7ac4fadf654f528f98bf4
Tianze <tankztz@gmail.com>
2025-03-08 12:39:52 +0800

    Implement webhook

--- viral-asia-blog/sanity.config.js
+++ viral-asia-blog/sanity.config.js
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import {webhooksTrigger} from 'sanity-plugin-webhooks-trigger'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +11,17 @@ export default defineConfig({
   projectId: '3an9f3n5',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    structureTool(), 
+    visionTool(),
+    webhooksTrigger({
+      urls: [{
+        url: 'https://api.cloudflare.com/client/v4/pages/webhooks/deploy_hooks/a281e103-a77e-4368-bb66-9028a472e71c',
+        triggerOn: 'create update delete',
+        name: 'Deploy to Cloudflare Pages'
+      }]
+    })
+  ],
 
   schema: {
     types: schemaTypes,
